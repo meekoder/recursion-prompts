645f85d8aa3b3504cea204278107e6d8a83e79de
meekoder <meekolydm@yahoo.com>
2020-09-08 15:54:53 -0700

    completed 2 - 4

--- src/recursion.js
+++ src/recursion.js
@@ -15,15 +15,30 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) return 0;
+  return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  if (array.length === 0) return 0;
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      sum += arraySum(array[i]);
+    } else {
+      sum += array[i];
+    }
+  }
+  return sum;
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n === 0) return true;
+  if (n === 1) return false;
+  return isEven(Math.abs(n) - 2);
 };
 
 // 5. Sum all integers below a given integer.
