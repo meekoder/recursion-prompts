807a3165240bc09e26b648995ef2edce87a30b79
meekoder <meekolydm@yahoo.com>
2020-09-09 17:15:09 -0700

    completed greatest common divisor

--- src/recursion.js
+++ src/recursion.js
@@ -154,6 +154,9 @@ let divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 let gcd = function(x, y) {
+  if (x < 0 || y < 0) return null;
+  if (!y) return x;
+  return gcd(y, (x % y))
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
